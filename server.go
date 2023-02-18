7652918d8e2ee547ea0385d17f68cb9a22509f6d
toniess <ignat.glushkov@mail.ru>
2023-02-18 14:17:24 +0300

    Login is working

--- server.go
+++ server.go
@@ -2,18 +2,36 @@ package main
 
 import (
 	// "fmt"
+
+	"log"
 	"net"
+
 	// "strconv"
+	"database/sql"
 	"strings"
+
+	_ "github.com/lib/pq"
 )
 
+var DB *sql.DB
+
+func connectDB() {
+	connStr := "user=toniess password=123456 dbname=mtuci_chat sslmode=disable"
+	DB, _ = sql.Open("postgres", connStr)
+	if err := DB.Ping(); err != nil {
+		log.Panic(err)
+	}
+}
+
+func authUser(login, hashedPassword string) bool {
+	id := 0
+	err := DB.QueryRow("SELECT id FROM users WHERE login=$1 AND password=$2", login, hashedPassword).Scan(&id)
+	return err == nil
+}
 
 func main() {
-	go startServer()
-
-	for {
-
-	}
+	connectDB()
+	startServer()
 }
 
 func startServer() {
@@ -40,6 +58,10 @@ func handle(con net.Conn) {
 	defer con.Close()
 	// create new channel to send replies
 	replyChan := make(chan string)
+
+	//points if user has authorized
+	userAuthorized := false
+
 	// get new client requests in loop in new thread
 	go func() {
 		for {
@@ -54,13 +76,21 @@ func handle(con net.Conn) {
 		}
 	}()
 
+	if userAuthorized {
+		log.Fatal()
+	}
+
 	//sending replies to client in the loop
 	for {
 		select {
-		// case <-need_location:
-			// con.Write([]byte("GIVEMELOCATION#"))
-		// case reply := <-replyChan:
-			// location <- reply
+		case reply := <-replyChan:
+			switch messageType(reply) {
+
+			case "login":
+				userAuthorized = (messageBody(reply) == "ok")
+				con.Write([]byte(reply))
+			}
+
 		}
 	}
 }
@@ -68,11 +98,36 @@ func handle(con net.Conn) {
 // Requests may come together
 // so we need to split it to single ones
 func parseRequest(request string, replyChan chan string) {
+
 	//requests are separated with '#'
 	requestList := strings.Split(request, "#")
-	for _, singleRequest := range requestList {
-		if singleRequest != "" {
-			replyChan <- singleRequest
+
+	for _, request := range requestList {
+
+		switch messageType(request) {
+
+		///
+		///
+		///
+		///
+		case "login":
+			requestBoby := messageBody(request)
+			login := strings.Split(requestBoby, "|")[0]
+			passwordHash := strings.Split(requestBoby, "|")[1]
+			if authUser(login, passwordHash) {
+				replyChan <- "login:ok"
+			} else {
+				replyChan <- "login:fail"
+			}
+
 		}
 	}
+}
+
+func messageType(message string) string {
+	return strings.Split(message, ":")[0]
+}
+
+func messageBody(message string) string {
+	return strings.Split(message, ":")[1]
 }
